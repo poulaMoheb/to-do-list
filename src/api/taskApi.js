4eed05969ef852ad1293dafd216a8b45ff2dbf16
pmoheb <pmoheb@isolvedhcm.com>
2026-02-25 19:50:41 +0200

    add json-server dependency and server script to package.json, create db.json with initial tasks, and implement basic CRUD operations in taskApi.js

---
+++ src/api/taskApi.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:4000/tasks";
+
+export const fetchTasks = async (page,limit) => {
+    const res = await axios.get(`${API_BASE_URL}_page=${page}&_limit=${limit}`);
+}
+
+export const createTask = async (task) => {
+    const res = await axios.post(API_BASE_URL, task);
+    return res.data;
+}
+
+export const updateTask = async (id, updatedTask) => {
+    const res = await axios.put(`${API_BASE_URL}/${id}`, updatedTask);
+    return res.data;
+}
+
+export const deleteTask = async (id) => {
+    await axios.delete(`${API_BASE_URL}/${id}`);
+}
+
